0982a71c152d837687e09a2ac1971e11af5f5bb8
Priyaskd108 <138877791+Priyaskd108@users.noreply.github.com>
2023-11-21 14:29:43 -0300

    Add files via upload

---
+++ marvel/e_commerce/api/urls.py
@@ -0,0 +1,35 @@
+from django.urls import path, include
+from e_commerce.marvel_views import *
+
+# Importamos las API_VIEWS:
+from e_commerce.api.views import *
+from e_commerce.api.viewsets import *
+from rest_framework.routers import DefaultRouter
+
+urlpatterns = [
+    # Comic Function API View:
+    path('comic-list/', comic_list_api_view),
+    path('comic-retrieve/', comic_retrieve_api_view),
+    path('comic-create/', comic_create_api_view),
+    path('login/', LoginUserAPIView.as_view()),
+    # Comic Class API View:
+    path('comics/list/', GetComicAPIView.as_view()),
+    path('comics/<int:pk>/', GetOneComicAPIView.as_view()),
+    path(
+        'comics/comic/<int:marvel_id>/',
+        GetOneMarvelComicAPIView.as_view()
+    ),
+    path('comics/create/', PostComicAPIView.as_view()),
+    path('comics/list-create/', ListCreateComicAPIView.as_view()),
+    path('comics/update/<int:marvel_id>/', UpdateComicAPIView.as_view()),
+    path(
+        'comics/retrieve-update/<int:pk>/',
+        RetrieveUpdateComicAPIView.as_view()
+    ),
+    path('comics/delete/<int:pk>/', DestroyComicAPIView.as_view()),
+    # User API Viewsets:
+    #path('users/', include('e_commerce.api.routers')),
+    path('e-commerce/api/comics/user/<str:username>/', ComicUserAPIView.as_view(), name='comic_list_user'),
+    path('user-wish/', include('e_commerce.api.routers')),
+    
+]
